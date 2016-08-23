423c859f4777155788d8e8847fe757f2658ebb0b
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-08-23 08:49:02 -0500

    37th example of Pre-Test: Thread.join method example

---
+++ src/main/java/org/developersdelicias/pretest/Worker.java
@@ -0,0 +1,8 @@
+package org.developersdelicias.pretest;
+
+public class Worker extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
+}
